7a3f6006f05e832c687e606fd78efef96ccbaa86
Vinicius KOncicoski <viniciuskoncicoski@gmail.com>
2023-12-14 10:13:52 -0300

    up

--- main.js
+++ main.js
@@ -5,10 +5,226 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader.js';
 
 
-import stars from './public/star.jpg'
-import nebula from './public/nebula.jpg'
+import stars from '/star.jpg'
+import nebula from '/nebula.jpg'
 
 const clayUrl = new URL('./assets/clay.glb', import.meta.url);
+
+
+
+const KEYS = {
+    'a': 65,
+    's': 83,
+    'w': 87,
+    'd': 68,
+};
+  
+function clamp(x, a, b) {
+return Math.min(Math.max(x, a), b);
+}
+  
+class InputController {
+    constructor(target) {
+      this.target_ = target || document;
+      this.initialize_();    
+    }
+  
+    initialize_() {
+      this.current_ = {
+        leftButton: false,
+        rightButton: false,
+        mouseXDelta: 0,
+        mouseYDelta: 0,
+        mouseX: 0,
+        mouseY: 0,
+      };
+      this.previous_ = null;
+      this.keys_ = {};
+      this.previousKeys_ = {};
+      this.target_.addEventListener('mousedown', (e) => this.onMouseDown_(e), false);
+      this.target_.addEventListener('mousemove', (e) => this.onMouseMove_(e), false);
+      this.target_.addEventListener('mouseup', (e) => this.onMouseUp_(e), false);
+      this.target_.addEventListener('keydown', (e) => this.onKeyDown_(e), false);
+      this.target_.addEventListener('keyup', (e) => this.onKeyUp_(e), false);
+    }
+  
+    onMouseMove_(e) {
+      this.current_.mouseX = e.pageX - window.innerWidth / 2;
+      this.current_.mouseY = e.pageY - window.innerHeight / 2;
+  
+      if (this.previous_ === null) {
+        this.previous_ = {...this.current_};
+      }
+  
+      this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+      this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+    }
+  
+    onMouseDown_(e) {
+      this.onMouseMove_(e);
+  
+      switch (e.button) {
+        case 0: {
+          this.current_.leftButton = true;
+          break;
+        }
+        case 2: {
+          this.current_.rightButton = true;
+          break;
+        }
+      }
+    }
+  
+    onMouseUp_(e) {
+      this.onMouseMove_(e);
+  
+      switch (e.button) {
+        case 0: {
+          this.current_.leftButton = false;
+          break;
+        }
+        case 2: {
+          this.current_.rightButton = false;
+          break;
+        }
+      }
+    }
+  
+    onKeyDown_(e) {
+      this.keys_[e.keyCode] = true;
+    }
+  
+    onKeyUp_(e) {
+      this.keys_[e.keyCode] = false;
+    }
+  
+    key(keyCode) {
+      return !!this.keys_[keyCode];
+    }
+  
+    isReady() {
+      return this.previous_ !== null;
+    }
+  
+    update(_) {
+      if (this.previous_ !== null) {
+        this.current_.mouseXDelta = this.current_.mouseX - this.previous_.mouseX;
+        this.current_.mouseYDelta = this.current_.mouseY - this.previous_.mouseY;
+  
+        this.previous_ = {...this.current_};
+      }
+    }
+};
+  
+  
+class FirstPersonCamera {
+    constructor(camera, objects) {
+        this.camera_ = camera;
+        this.input_ = new InputController();
+        this.rotation_ = new THREE.Quaternion();
+        this.translation_ = new THREE.Vector3(0, 2, 0);
+        this.phi_ = 0;
+        this.phiSpeed_ = 8;
+        this.theta_ = 0;
+        this.thetaSpeed_ = 5;
+        this.headBobActive_ = false;
+        this.headBobTimer_ = 0;
+        this.objects_ = objects;
+    }
+
+    update(timeElapsedS) {
+        this.updateRotation_(timeElapsedS);
+        this.updateCamera_(timeElapsedS);
+        this.updateTranslation_(timeElapsedS);
+        //this.updateHeadBob_(timeElapsedS);
+        this.input_.update(timeElapsedS);
+    }
+
+    updateCamera_(_) {
+        this.camera_.quaternion.copy(this.rotation_);
+        this.camera_.position.copy(this.translation_);
+        this.camera_.position.y += Math.sin(this.headBobTimer_ * 10) * 1.5;
+
+        const forward = new THREE.Vector3(0, 0, -1);
+        forward.applyQuaternion(this.rotation_);
+
+        const dir = forward.clone();
+
+        forward.multiplyScalar(100);
+        forward.add(this.translation_);
+
+        let closest = forward;
+        const result = new THREE.Vector3();
+        const ray = new THREE.Ray(this.translation_, dir);
+        for (let i = 0; i < this.objects_.length; ++i) {
+        if (ray.intersectBox(this.objects_[i], result)) {
+            if (result.distanceTo(ray.origin) < closest.distanceTo(ray.origin)) {
+            closest = result.clone();
+            }
+        }
+        }
+
+        this.camera_.lookAt(closest);
+    }
+
+    updateHeadBob_(timeElapsedS) {
+        if (this.headBobActive_) {
+        const wavelength = Math.PI;
+        const nextStep = 1 + Math.floor(((this.headBobTimer_ + 0.000001) * 10) / wavelength);
+        const nextStepTime = nextStep * wavelength / 10;
+        this.headBobTimer_ = Math.min(this.headBobTimer_ + timeElapsedS, nextStepTime);
+
+        if (this.headBobTimer_ == nextStepTime) {
+            this.headBobActive_ = false;
+        }
+        }
+    }
+
+    updateTranslation_(timeElapsedS) {
+        const forwardVelocity = (this.input_.key(KEYS.w) ? 1 : 0) + (this.input_.key(KEYS.s) ? -1 : 0)
+        const strafeVelocity = (this.input_.key(KEYS.a) ? 1 : 0) + (this.input_.key(KEYS.d) ? -1 : 0)
+
+        const qx = new THREE.Quaternion();
+        qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
+
+        const forward = new THREE.Vector3(0, 0, -1);
+        forward.applyQuaternion(qx);
+        forward.multiplyScalar(forwardVelocity * timeElapsedS * 10);
+
+        const left = new THREE.Vector3(-1, 0, 0);
+        left.applyQuaternion(qx);
+        left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
+
+        this.translation_.add(forward);
+        this.translation_.add(left);
+
+        if (forwardVelocity != 0 || strafeVelocity != 0) {
+        this.headBobActive_ = true;
+        }
+    }
+
+    updateRotation_(timeElapsedS) {
+        const xh = this.input_.current_.mouseXDelta / window.innerWidth;
+        const yh = this.input_.current_.mouseYDelta / window.innerHeight;
+
+        this.phi_ += -xh * this.phiSpeed_;
+        this.theta_ = clamp(this.theta_ + -yh * this.thetaSpeed_, -Math.PI / 3, Math.PI / 3);
+
+        const qx = new THREE.Quaternion();
+        qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi_);
+        const qz = new THREE.Quaternion();
+        qz.setFromAxisAngle(new THREE.Vector3(1, 0, 0), this.theta_);
+
+        const q = new THREE.Quaternion();
+        q.multiply(qx);
+        q.multiply(qz);
+
+        this.rotation_.copy(q);
+    }
+}
+  
+/* AUX */
+let previousRAF_ = null;
 
 
 const renderer = new THREE.WebGLRenderer();
@@ -17,11 +233,34 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const fov = 60;
+const aspect = 1920 / 1080;
+const near = 1.0;
+const far = 1000.0;
+const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+camera.position.set(0, 2, 0);
 
-const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 10, 20);
-orbit.update();
+const fps_camera = new FirstPersonCamera(camera, []);
+
+const uiCamera_ = new THREE.OrthographicCamera(
+    -1, 1, 1 * aspect, -1 * aspect, 1, 1000);
+const uiScene_ = new THREE.Scene();
+
+// Crosshair
+const maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
+const mapLoader = new THREE.TextureLoader();
+const crosshair = mapLoader.load('/crosshair.png');
+crosshair.anisotropy = maxAnisotropy;
+
+const sprite_ = new THREE.Sprite(
+  new THREE.SpriteMaterial({map: crosshair, color: 0xffffff, fog: false, depthTest: false, depthWrite: false}));
+sprite_.scale.set(0.10, 0.15 *camera.aspect, 1)
+sprite_.position.set(0, 0, -10);
+
+uiScene_.add(sprite_);
+
+//const orbit = new OrbitControls(camera, renderer.domElement);
+//orbit.update();
 
 /* Helpers */
 const axesHelper = new THREE.AxesHelper(10);
@@ -29,11 +268,12 @@ scene.add(axesHelper);
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-/* Cube */
+/* Cube 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
+*/
 
 /* Plane */
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
@@ -174,10 +414,9 @@ assetLoader.load(
     clayUrl.href,
     gltf => {
         const model = gltf.scene;
-        model.position.set(-12, 0, 10);
+        model.position.set(0, 0, 10);
         //model.scale.set(0.01, 0.01, 0.01);
         scene.add( model );
-        
         mixer = new THREE.AnimationMixer(model);
         const clips = gltf.animations;
         const clip = THREE.AnimationClip.findByName(clips, 'idle');
@@ -201,8 +440,9 @@ function animate(time) {
     if (mixer !== undefined) {
         mixer.update(clock.getDelta());
     }
-	box.rotation.x = time / 1000;
-    box.rotation.y = time / 1000;
+
+    //box.rotation.x = time / 1000;
+    //box.rotation.y = time / 1000;
 
     step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step));
@@ -226,13 +466,31 @@ function animate(time) {
         }
     })
 
+    requestAnimationFrame((t) => {
+        if (previousRAF_ === null) {
+            previousRAF_ = t;
+        }
+
+        step_(t - previousRAF_);
+        previousRAF_ = t;
+    });
+    renderer.autoClear = true;
 	renderer.render( scene, camera );
+    renderer.autoClear = false;
+    renderer.render (uiScene_, uiCamera_);
+}
+
+function step_(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001;
+
+    // this.controls_.update(timeElapsedS);
+    fps_camera.update(timeElapsedS);
 }
 
 renderer.setAnimationLoop(animate);
 
 window.addEventListener('resize', e => {
     camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    camera.updateProjectionMatrix();  
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
